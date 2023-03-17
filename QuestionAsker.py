0b9d44bc7d763bf2e58dfdd6bc66ce804b46ea9b
mathias <amkpapadakis@gmail.com>
2023-03-17 14:49:08 +0100

    Program giver spørgsmål, og spiller giver svar (test)

---
+++ QuestionAsker.py
@@ -0,0 +1,29 @@
+def questionasker()
+
+## question
+
+def my_function(fname):
+
+  print(fname)
+
+my_function("a: ch")
+my_function("b: dk")
+my_function("c: ca")
+my_function("d: pl")
+
+
+##answers
+
+questionone = input("What country code does Switzerland use?")
+
+if questionone == "a":
+    print("Correct!")
+
+elif questionone == "b":
+    exit("Wrong answer!")
+
+elif questionone == "c":
+    exit("Wrong answer!")
+
+elif questionone == "d":
+    exit("Wrong answer!")
